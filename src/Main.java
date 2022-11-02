41390c7eb201d56379158f90ffe621e588a9de6b
Elif Savaş <98321352+elifsavas@users.noreply.github.com>
2022-11-03 00:39:11 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int k ;
+        double result = 0 ;
+
+        Scanner input = new Scanner(System.in) ;
+
+        System.out.print("Sayi Giriniz: ");
+        k = input.nextInt();
+
+        for (int i=1 ; i<=k ; i++) {
+
+            result += (1.0/i) ; //int ile int bölündüğünde int değer çıkar. double, int'a bölündüğünde double çıkar.
+                                // veya i'yi double ile tanımlayabiliriz.
+
+        }
+
+        System.out.println(result);
     }
 }
